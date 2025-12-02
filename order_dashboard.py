10aff356554221b989be5849f314967771e74f9e
dravya1311 <yravii.1311@gmail.com>
2025-12-02 14:07:02 +0530

    Update order_dashboard.py

--- order_dashboard.py
+++ order_dashboard.py
@@ -281,7 +281,7 @@ else:
 # -------------------------------------------------------------
 st.subheader("Top 10 Most Delayed Routes")
 # Combine origin and destination
-st.write("COLUMNS:", df_view.columns.tolist())
+
 df_view["origin"] = df_view["order_city"].astype(str) + ", " + df_view["order_country"].astype(str)
 df_view["destination"] = df_view["customer_city"].astype(str) + ", " + df_view["customer_country"].astype(str)
 

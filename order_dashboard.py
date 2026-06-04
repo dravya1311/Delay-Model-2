33eb0377cb5b075eb51eddb3ec2052756e377239
dravya1311 <yravii.1311@gmail.com>
2026-06-04 19:11:21 +0530

    Update order_dashboard.py

--- order_dashboard.py
+++ order_dashboard.py
@@ -92,8 +92,8 @@ delay_region = (
 fig = px.bar(
     delay_region,
     x="order_region",
-    y="delay_pct",
-    text=delay_region["delay_pct"].astype(int),
+    yaxis_title="Delay (%)"
+   text=delay_region["delay_pct"].astype(int)
     title="Delay % by Region"
 )
 
